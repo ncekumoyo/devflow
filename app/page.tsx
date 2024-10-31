96449c2223768a4128e5cbd60519d790b8c5fe3a
Nceku Moyo <nhmoyo7@gmail.com>
2024-10-31 15:12:01 +0200

    ESLint setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="text-3xl text-violet-700 font-black">Welcome to the world of NextJS</h1>;
+  return <h1 className="text-3xl font-black text-violet-700">Welcome to the world of NextJS</h1>;
 }
