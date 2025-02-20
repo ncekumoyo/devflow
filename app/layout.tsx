ac9434582a67c979a96215222b31c642f7220d95
ncekumoyo <nhmoyo7@gmail.com>
2025-02-20 15:10:43 +0200

    Setup light and dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import ThemeProvider from "@/context/Theme";
+import Navbar from "@/components/ui/navigation/navbar";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -37,6 +38,7 @@ export default function RootLayout({
         className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
         <ThemeProvider attribute="class"> 
+          <Navbar />
         {children}
         </ThemeProvider>
       </body>
